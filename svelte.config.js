dc98554b5c2d51ce6b7c8282a6a872db115b146c
diamondburned <diamond@arikawa-hi.me>
2023-06-20 20:58:12 -0700

    Use adapterAuto for Netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,16 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import preprocess from "svelte-preprocess"
+import adapterAuto from "@sveltejs/adapter-auto"
+import adapterStatic from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/kit/vite"
+
+// isCloud is true if the app is being deployed to a cloud provider.
+// Keep this up to date with adapterAuto's list of cloud providers:
+// https://github.com/sveltejs/kit/blob/master/packages/adapter-auto/adapters.js
+const isCloud =
+  !!process.env.VERCEL ||
+  !!process.env.NETLIFY ||
+  !!process.env.CF_PAGES ||
+  process.env.GITHUB_ACTION_REPOSITORY === "Azure/static-web-apps-deploy"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,15 +21,17 @@ const config = {
     }),
   ],
   kit: {
-    adapter: adapter({
-      fallback: "index.html",
-      precompress: true,
-      strict: true,
-    }),
+    adapter: isCloud
+      ? adapterAuto()
+      : adapterStatic({
+          fallback: "index.html",
+          precompress: true,
+          strict: true,
+        }),
     alias: {
       "#": "./src",
     },
   },
-};
+}
 
-export default config;
+export default config
