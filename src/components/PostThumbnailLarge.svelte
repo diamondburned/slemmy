3d4608b4684473938b290577e08639049b10d829
diamondburned <diamond@arikawa-hi.me>
2023-06-23 00:24:39 -0700

    Fix PostThumbnailLarge modal regression

--- src/components/PostThumbnailLarge.svelte
+++ src/components/PostThumbnailLarge.svelte
@@ -9,15 +9,16 @@
 
   let className = ""
   export { className as class }
+  export let imageClass = ""
 
   let loaded = false
   let imageElement: HTMLImageElement
 </script>
 
 {#if thumbnailURL}
-  <div class="rounded m-auto w-full max-w-lg max-h-lg">
+  <div class="rounded m-auto w-full max-w-lg max-h-lg {className}">
     <img
-      class="rounded max-w-full max-h-full duration-100 transition-opacity {className}"
+      class="rounded max-w-full max-h-full duration-100 transition-opacity {imageClass}"
       class:absolute={!loaded}
       class:opacity-0={!loaded}
       on:load={() => (loaded = true)}
